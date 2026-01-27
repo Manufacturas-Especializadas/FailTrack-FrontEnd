405c1581f55939a322315897561d7aa273be9068
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-27 14:35:54 -0600

    Endpoint para obtener los estatus

--- src/config/api.ts
+++ src/config/api.ts
@@ -14,6 +14,7 @@ export const API_CONFIG = {
       update: "/api/Maintenance/Update/",
     },
     generalList: {
+      getStatus: "/api/GeneralList/GetStatus",
       getLines: "/api/GeneralList/GetLines",
       getMachinesByLine: "/api/GeneralList/GetMachineByLine/",
     },
