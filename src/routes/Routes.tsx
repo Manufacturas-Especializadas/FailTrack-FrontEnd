d71249b7c8b3ab19c6af3fd1e587fb8a6f6cc021
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-28 09:35:55 -0600

    Ruta herramentales

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { MaintenanceIndex } from "../pages/Maintenance/MaintenanceIndex";
 import { Home } from "../pages/Home/Home";
 import { DashboardMaintenance } from "../pages/MaintenanceAdministrator/DashboardMaintenance";
+import { ToolingIndex } from "../pages/Tooling/ToolingIndex";
 
 export const MyRoutes = () => {
   return (
@@ -13,6 +14,8 @@ export const MyRoutes = () => {
         path="/dasboard-mantenimiento-admin"
         element={<DashboardMaintenance />}
       />
+
+      <Route path="/herramentales" element={<ToolingIndex />} />
     </Routes>
   );
 };
