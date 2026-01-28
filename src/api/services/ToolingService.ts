1264c06689a969f4edd62fc1e895d2e7794bbf38
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-28 11:43:37 -0600

    Metodo para obtener lista de solicitudes - Tooling

--- src/api/services/ToolingService.ts
+++ src/api/services/ToolingService.ts
@@ -1,4 +1,5 @@
 import { API_CONFIG } from "../../config/api";
+import type { TicketsTooling } from "../../types/TicketsTooling";
 import type { Tooling } from "../../types/Tooling";
 import { apiClient } from "../client";
 
@@ -8,9 +9,14 @@ interface Response {
 }
 
 class ToolingService {
+  private getToolingListEndpoint = API_CONFIG.endpoints.tooling.getToolingList;
   private getToolingByIdEndpoint = API_CONFIG.endpoints.tooling.getToolingById;
   private createEndpoint = API_CONFIG.endpoints.tooling.create;
   private updateEndpoint = API_CONFIG.endpoints.tooling.update;
+
+  async getToolingList(): Promise<TicketsTooling[]> {
+    return apiClient.get<TicketsTooling[]>(this.getToolingListEndpoint);
+  }
 
   async getToolingById(id: number): Promise<Tooling> {
     return apiClient.get(`${this.getToolingByIdEndpoint}${id}`);
