0a8db7e46e319f9bdb804d23fffb97448e5134f2
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-28 09:47:51 -0600

    Metodo para registrar tooling

--- src/api/services/ToolingService.ts
+++ src/api/services/ToolingService.ts
@@ -1,3 +1,18 @@
-class ToolingService {}
+import { API_CONFIG } from "../../config/api";
+import type { Tooling } from "../../types/Tooling";
+import { apiClient } from "../client";
+
+interface Response {
+  success: boolean;
+  message: string;
+}
+
+class ToolingService {
+  private createEndpoint = API_CONFIG.endpoints.tooling.create;
+
+  async create(formData: Tooling): Promise<Response> {
+    return apiClient.post<Response>(this.createEndpoint, formData);
+  }
+}
 
 export const toolingService = new ToolingService();
