10735f5db154019f92b3650a64540282a3302246
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-01-29 14:15:04 -0600

    Metodos para obtener y descargar reportes mensuales - Mantenimiento

--- src/api/services/MaintenanceService.ts
+++ src/api/services/MaintenanceService.ts
@@ -1,6 +1,6 @@
 import { API_CONFIG } from "../../config/api";
 import type { Maintenance } from "../../types/Maintenance";
-import type { ReportItem } from "../../types/ReportItem";
+import type { MonthlyReportItem } from "../../types/MonthlyReportItem";
 import type { Ticket } from "../../types/Ticket";
 import { apiClient } from "../client";
 
@@ -16,6 +16,8 @@ class MaintenanceService {
     API_CONFIG.endpoints.maintenance.getMaintenanceById;
   private getAvailableMontlyReportsEndpoint =
     API_CONFIG.endpoints.maintenance.getAvailableMonthlyReports;
+  private downloadReportEndpoint =
+    API_CONFIG.endpoints.maintenance.downloadMonthlyReport;
   private createEndpoint = API_CONFIG.endpoints.maintenance.create;
   private updateEndpoint = API_CONFIG.endpoints.maintenance.update;
 
@@ -27,8 +29,24 @@ class MaintenanceService {
     return apiClient.get(`${this.getMaintenanceByIdEndpoint}${id}`);
   }
 
-  async getAvailableMontlyReports(): Promise<ReportItem[]> {
-    return apiClient.get<ReportItem[]>(this.getAvailableMontlyReportsEndpoint);
+  async getAvailableMontlyReports(): Promise<MonthlyReportItem[]> {
+    return apiClient.get<MonthlyReportItem[]>(
+      this.getAvailableMontlyReportsEndpoint,
+    );
+  }
+
+  async downloadReport(year: number, month: number): Promise<void> {
+    const endpoint = `${this.downloadReportEndpoint}?year=${year}&month=${month}`;
+
+    const monthName = new Date(year, month - 1).toLocaleString("es-Mx", {
+      month: "long",
+    });
+
+    const formattedMonth =
+      monthName.charAt(0).toUpperCase() + monthName.slice(1);
+    const fileName = `Reporte_Mensual_${formattedMonth}_${year}.xlsx`;
+
+    return apiClient.downloadFile(endpoint, fileName);
   }
 
   async create(formData: Maintenance): Promise<ApiResponse> {
