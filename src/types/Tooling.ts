9a6818f47f58f31edfe3aac94dc0d8d6eeabdb2c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-20 08:52:10 -0600

    feat: se agregaron los campos de descripcion de falla y responsable

--- src/types/Tooling.ts
+++ src/types/Tooling.ts
@@ -1,6 +1,8 @@
 export interface Tooling {
   applicantName: string;
   faultDescription: string;
+  failureDescription: string;
+  responsible: string;
   idLine: number;
   idMachine: number;
   idStatus: number;
