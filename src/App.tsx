de9104de8a3ec8c7f7cbb0b7612773a006adb4dd
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-14 09:41:24 -0600

    feat: authprovider

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,52 @@ import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { MyRoutes } from "./routes/Routes";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Toaster
-        toastOptions={{
-          className: "",
-          style: {
-            background: "#363636",
-            color: "#fff",
-            zIndex: 9999,
-          },
-          success: {
-            duration: 10000,
-            position: "top-right",
+      <AuthProvider>
+        <Toaster
+          toastOptions={{
+            className: "",
             style: {
-              background: "#10B981",
+              background: "#363636",
               color: "#fff",
+              zIndex: 9999,
             },
-          },
-          error: {
-            duration: 5000,
-            position: "top-right",
-            style: {
-              background: "#EF4444",
-              color: "#fff",
+            success: {
+              duration: 5000,
+              position: "top-right",
+              style: {
+                background: "#10B981",
+                color: "#fff",
+              },
             },
-          },
-          loading: {
-            duration: Infinity,
-            position: "top-right",
-            style: {
-              background: "#3B82F6",
-              color: "#fff",
+            error: {
+              duration: 5000,
+              position: "top-right",
+              style: {
+                background: "#EF4444",
+                color: "#fff",
+              },
             },
-          },
-        }}
-      />
-      <Navbar />
-      <main>
-        <MyRoutes />
-      </main>
+            loading: {
+              position: "top-right",
+              style: {
+                background: "#3B82F6",
+                color: "#fff",
+              },
+            },
+          }}
+        />
+
+        <Navbar />
+
+        <main className="min-h-screen bg-slate-50">
+          <MyRoutes />
+        </main>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
