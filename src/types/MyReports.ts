4a6263b5025165af4ae5df2695eba92f03bb6f2e
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-14 10:06:13 -0600

    feat: interfaz reportes

---
+++ src/types/MyReports.ts
@@ -0,0 +1,12 @@
+export interface Reports {
+  id: number;
+  name: string;
+  description: string;
+  date: string;
+  line: string;
+  machine: string;
+  reponsible: string;
+  solution: string;
+  closingDate: string;
+  lineDescription: string;
+}
